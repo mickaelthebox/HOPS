23a9fea31f81ddddc634e294c5d50b4fba5f7793
mickaelthebox <mickael@the-box.fr>
2020-09-18 14:56:56 +0200

    destroy the orginal folder to support the integration aws

---
+++ build/precache-manifest.c3d7af0a792b5a2e8983c5ef8c22e732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3221c16d504e7dd8e1812bbe33979ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f88323a54e40ab0af54",
+    "url": "/static/js/2.6060ed93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6060ed93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6233517c6ea7a7734ca2",
+    "url": "/static/js/main.6cf47afb.chunk.js"
+  },
+  {
+    "revision": "f356b06a79eb0bbc29fc",
+    "url": "/static/js/runtime-main.964e7635.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  }
+]);
