033f7a7bc938feae9e9f0cf1448a5464981c3ef5
mickaelthebox <mickael@the-box.fr>
2020-09-18 13:14:42 +0200

    bootstraping of the project with reactjs and i18n

---
+++ hops-website/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTranslation, Trans } from 'react-i18next';
+function App() {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    }
+    return (
+        <div>
+            <h1>{t('title')}</h1>
+            <div> 
+                <button onClick={() => changeLanguage('fr-FR')} type="button">FR</button>
+                <button onClick={() => changeLanguage('en-US')} type="button">EN</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
